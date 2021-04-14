6211ebf3c78bd4efe6289c5a3585df7571387101
Pete Erickson <petee@mindtouch.com>
2021-04-14 00:17:34 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -1,9 +1,5 @@
 node {
     agent any
-    parameters {
-        string(name: 'GIT_REMOTE', defaultValue: 'MindTouch', description: 'Name of git repo')
-        string(name: 'GIT_BRANCH', defaultValue: 'master', description: 'Branch to build')
-    }
     stages {
         stage('Stage 1') {
             steps {
