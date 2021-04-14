23fd04cf33827cedf9cd7bd808a5e08ab78e9efc
Pete Erickson <petee@mindtouch.com>
2021-04-14 11:23:25 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -3,21 +3,25 @@ pipeline {
     stages {
         stage('SetCloudName') {
           steps {
-            script {
-              env.CLOUD_NAME = sh(script:'echo alpha', returnStdout: true).trim()
+            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'alpha-aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+    // some block
+              sh 'aws sts get-caller-identity'
+              /*
+              script {
+                env.CLOUD_NAME = sh(script:'echo alpha', returnStdout: true).trim()
+              }
+              */
             }
           }
         }
+        /*
         stage('checkout') {
             steps {
-                echo "${env.GIT_REMOTE}"
-                sh "printenv"
-                /*
                 checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${env.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
                 sh 'git remote -v'
                 sh 'git branch -a'
-                */
             }
         }
+        */
     }
 }
