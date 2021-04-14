8b2e2a1c35767b254ddac46cfbfbd2b77a09c300
Pete Erickson <petee@mindtouch.com>
2021-04-14 10:52:42 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -1,23 +1,13 @@
 pipeline {
     agent any
     environment {
-      CLOUD_NAME = ''
+      CLOUD_NAME = "${sh(script: 'echo alpha', returnStdout: true).trim()}"
     }
     stages {
-        stage('DetermineCloudName') {
-          steps {
-            script {
-              env.CLOUD_NAME = sh(
-                script: 'echo alpha',
-                returnStdout: true).trim()
-            }
-            sh 'env |grep CLOUD_NAME'
-          }
-        }
         stage('checkout') {
             steps {
                 echo "${env.GIT_REMOTE}"
-                sh "env"
+                sh "printenv"
                 /*
                 checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${env.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
                 sh 'git remote -v'
