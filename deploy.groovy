5749b054eb9b592442ba25da436a4c02d5426eac
Pete Erickson <petee@mindtouch.com>
2021-04-14 09:38:51 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -5,9 +5,9 @@ pipeline {
             steps {
                 echo "${env.GIT_REMOTE}"
                 sh "env"
-                // checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${params.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${params.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
-                // sh "git remote -v"
-                // sh "git branch -a"
+                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${env.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
+                sh 'git remote -v'
+                sh 'git branch -a'
             }
         }
     }
