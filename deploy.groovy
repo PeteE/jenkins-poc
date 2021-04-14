1a2387b931647b367c73f8af4ecb5b41865e8c79
Pete Erickson <petee@mindtouch.com>
2021-04-14 10:30:28 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -12,17 +12,17 @@ pipeline {
             sh 'env |grep CLOUD_NAME'
           }
         }
+/*
         stage('checkout') {
             steps {
                 echo 'CLOUD_NAME=${CLOUD_NAME}'
-                /*
                 echo "${env.GIT_REMOTE}"
                 sh "env"
                 checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${env.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
                 sh 'git remote -v'
                 sh 'git branch -a'
-                */
             }
         }
+*/
     }
 }
