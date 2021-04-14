44da564870a804eafeda0a4a1b00ef6e1c6fc33e
Pete Erickson <petee@mindtouch.com>
2021-04-14 09:36:29 -0500

    testing

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,5 @@
 def branch = this.getProperty('binding').getVariable('GIT_BRANCH')
 
-
 def create_job(String _remote='MindTouch', String _branch) {
   pipelineJob('petetest_pipeline_' + _branch) {
     definition {
--- deploy.groovy
+++ deploy.groovy
@@ -3,11 +3,11 @@ pipeline {
     stages {
         stage('checkout') {
             steps {
-                echo "${params.GIT_REMOTE}"
-                echo "${params.GIT_BRANCH}"
-                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${params.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${params.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
-                sh "git remote -v"
-                sh "git branch -a"
+                echo "${env.GIT_REMOTE}"
+                sh "env"
+                // checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${params.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${params.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
+                // sh "git remote -v"
+                // sh "git branch -a"
             }
         }
     }
