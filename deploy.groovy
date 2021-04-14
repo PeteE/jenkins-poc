f51ea209a8a0d945512dadcf4ee1ab856562090c
Pete Erickson <petee@mindtouch.com>
2021-04-14 00:51:51 -0500

    testing

--- deploy.groovy
+++ deploy.groovy
@@ -5,6 +5,7 @@ pipeline {
             steps {
                 echo "${params.GIT_REMOTE}"
                 echo "${params.GIT_BRANCH}"
+                checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: "${params.GIT_BRANCH}"]], extensions: [], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
             }
         }
     }
