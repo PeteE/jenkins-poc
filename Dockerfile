bd8119992bb0c46b51dd9448ca00073ed59a7d5a
Pete Erickson <petee@mindtouch.com>
2021-05-19 16:08:47 -0500

    testing

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM jenkins/jenkins:lts
+
+USER root
+####### INSTALL DOCKER CLI
+RUN set -eux; \
+  apt-get update; \
+  apt-get install -y --no-install-recommends \
+    gnupg2 \
+    software-properties-common; \
+  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -; \
+  add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"; \
+  apt-get update; \
+  apt-get install -y --no-install-recommends \
+    docker-ce-cli \
+    containerd.io; \
+  rm -rf /var/lib/apt/lists/*
+USER jenkins
+COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
+RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
--- deploy.groovy
+++ deploy.groovy
@@ -1,27 +1,10 @@
 pipeline {
     agent any
     stages {
-        stage('SetCloudName') {
+        stage('test') {
           steps {
-            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'alpha-aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-    // some block
-              sh 'aws sts get-caller-identity'
-              /*
-              script {
-                env.CLOUD_NAME = sh(script:'echo alpha', returnStdout: true).trim()
-              }
-              */
-            }
+            echo 'here'
           }
         }
-        /*
-        stage('checkout') {
-            steps {
-                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.GIT_BRANCH}"]], extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'LocalBranch', localBranch: "${env.GIT_BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/PeteE/jenkins-poc']]]
-                sh 'git remote -v'
-                sh 'git branch -a'
-            }
-        }
-        */
     }
 }
